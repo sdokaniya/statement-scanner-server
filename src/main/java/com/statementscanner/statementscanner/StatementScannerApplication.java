e2dd6f4cec4c3634f4e258385b084d9ac2035415
sdokaniya <sdokaniya@gmail.com>
2024-02-10 23:54:55 +0530

    Initial project commit

---
+++ src/main/java/com/statementscanner/statementscanner/StatementScannerApplication.java
@@ -0,0 +1,13 @@
+package com.statementscanner.statementscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatementScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/statementscanner/statementscanner/StatementScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statementscanner.statementscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
